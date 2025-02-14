c7ba2489c52f872a8ab3b2f82149a3c48851eed3
richgagamidush <agagamidush@gmail.com>
2025-02-14 23:59:03 +0300

    Update zokou.js

--- framework/zokou.js
+++ framework/zokou.js
@@ -6,7 +6,7 @@ function zokou(obj, fonctions) {
         infoComs.categorie = "General";
     }
     if (!obj.reaction) {
-        infoComs.reaction = "✅";
+        infoComs.reaction = "🚓";
     }
     infoComs.fonction = fonctions;
     cm.push(infoComs);
