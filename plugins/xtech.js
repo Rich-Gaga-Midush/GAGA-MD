3cc7065518ebf10e972550af50ab02b7f054a46f
richgagamidush <agagamidush@gmail.com>
2025-02-21 07:21:55 +0300

    Update xtech.js

--- plugins/xtech.js
+++ plugins/xtech.js
@@ -91,11 +91,11 @@ zokou({ nomCom: "menu", categorie: "General" }, async (dest, zk, commandeOptions
 ╭━━━〔 𝐗𝐆𝐀𝐆𝐀 𝐁𝐎𝐓𝐒 〕━━━
 ┌════════════════⊷
 ╭─────────────────
-  *Bot name :* XGAGA BOTS
- ✣ *User:*  ${nomAuteurMessage}  
- ✣ *Time :*  ${temps}
- ✣ *Date :* ${date} 
- ✣ *Uptime :*  ${runtime(process.uptime())}  
+ ࿇ *Bot name :* XGAGA BOTS
+ ࿇ *User:*  ${nomAuteurMessage}  
+ ࿇ *Time :*  ${temps}
+ ࿇ *Date :* ${date} 
+ ࿇ *Uptime :*  ${runtime(process.uptime())}  
 └───────────────────
  ━〔 𝐊𝐄𝐄𝐏 𝐔𝐒𝐈𝐍𝐆 𝐆𝐀𝐆𝐀 𝐌𝐃 〕━`;
 
@@ -149,107 +149,3 @@ zokou({ nomCom: "menu", categorie: "General" }, async (dest, zk, commandeOptions
     }
 });
 
-zokou({ nomCom: "list", categorie: "General" }, async (dest, zk, commandeOptions) => {
-    let { ms, repondre, prefixe, nomAuteurMessage } = commandeOptions;
-    let { cm } = require(__dirname + "/../framework/zokou");
-    var coms = {};
-    var mode = "public";
-
-    if ((s.MODE).toLocaleLowerCase() != "public") {
-        mode = "Private";
-    }
-
-    // Normalize category to uppercase and organize commands by category
-    cm.map(async (com, index) => {
-        const categoryUpper = com.categorie.toUpperCase();
-        if (!coms[categoryUpper])
-            coms[categoryUpper] = [];
-        coms[categoryUpper].push(com.nomCom);
-    });
-
-    // Set the default timezone from the configuration
-    moment.tz.setDefault('Africa/Nairobi');
-
-    // Create a date and time in the configured timezone
-    const temps = moment().format('HH:mm:ss');
-    const date = moment().format('DD/MM/YYYY');
-
-    // Determine the greeting based on the current time
-    const hour = moment().hour();
-    let greeting = "ʟᴇᴛ's ᴋɪᴄᴋsᴛᴀʀᴛ ʏᴏᴜʀ ᴅᴀʏ ✨";
-    if (hour >= 0 && hour <= 11) {
-        greeting = "ɢᴏᴏᴅ ᴍᴏʀɴɪɴɢ 🌄, ʟᴇᴛ's ᴋɪᴄᴋsᴛᴀʀᴛ ʏᴏᴜʀ ᴅᴀʏ ✨";
-    } else if (hour >= 12 && hour <= 16) {
-        greeting = "ɢᴏᴏᴅ ᴀғᴛᴇʀɴᴏᴏɴ 🌅, ʜᴏᴡ ɪs ʏᴏᴜʀ ᴅᴀʏ ɢᴏɪɴɢ 🎍";
-    } else if (hour >= 16 && hour <= 21) {
-        greeting = "ɢᴏᴏᴅ ᴇᴠᴇɴɪɴɢ 🌃, ɪ ʜᴏᴘᴇ ʏᴏᴜʀ ᴅᴀʏ ᴡᴀs ɢᴏᴏᴅ 🦋 ";
-    } else if (hour >= 21 && hour <= 23) {
-        greeting = "ɢᴏᴏᴅ ɴɪɢʜᴛ🌘, sᴡᴇᴇᴛ ᴅʀᴇᴀᴍs 💫";
-    }
-
-    // Fetch GitHub stats
-    const { totalUsers } = await fetchGitHubStats();
-    const formattedTotalUsers = totalUsers.toLocaleString();
-
-    let infoMsg = `
-> *${greeting}*
-
-┌════════════════⊷
-┊✣╭─────────────
-┊✣┊ *Bot name :* ʙᴇʟᴛᴀʜ xʙᴏᴛ
-┊✣┊ *User:*  ${nomAuteurMessage}  
-┊✣┊ *Time :*  ${temps}
-┊✣┊ *Date :* ${date} 
-┊✣┊ *Uptime :*  ${runtime(process.uptime())}  
-┊✣└───────────────
-╰═════════════════⊷`;
-
-    let menuMsg =`
-> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʙᴇʟᴛᴀʜ ʜᴀᴄᴋɪɴɢ ᴛᴇᴀᴍ\n${readmore} `;
-
-    // Sort categories alphabetically and generate menu
-    const sortedCategories = Object.keys(coms).sort();
-    let commandNumber = 1; 
-
-    for (const cat of sortedCategories) {
-        menuMsg += `
-╭━━━〔  ${cat.toUpperCase()} 〕━━━┈⊷
-┊✣╭──────────────`;
-
-        // Sort commands alphabetically within the category
-        const sortedCommands = coms[cat].sort();
-
-        for (const cmd of sortedCommands) {
-            menuMsg += ` 
-┊✣┊${cmd}`;
-        }
-        menuMsg += `
-┊✣╰──────────────
-╰═════════════════⊷\n`;
-    }
-
-    menuMsg += `
-> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʙᴇʟᴛᴀʜ ʜᴀᴄᴋɪɴɢ ᴛᴇᴀᴍ 👻`;
-
-   try {
-        await zk.sendMessage(dest, { 
-            text: infoMsg + menuMsg,
-            contextInfo: {
-                mentionedJid: [nomAuteurMessage],
-               showAdAttribution: true,
-                externalAdReply: {
-                    title: "𝐁𝐄𝐋𝐓𝐀𝐇 𝐗𝐁𝐎𝐓",
-                    body: "ᴛᴀᴘ ʜᴇʀᴇ ᴛᴏ ғᴏʟʟᴏᴡ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ",
-                    thumbnailUrl: "https://telegra.ph/file/dcce2ddee6cc7597c859a.jpg",
-                    sourceUrl: 'https://whatsapp.com/channel/0029VaRHDBKKmCPKp9B2uH2F',
-                    mediaType: 1,
-                    renderLargerThumbnail: true
-                }
-            }
-        }); 
-    
-    } catch (e) {
-        console.log("🥵🥵 Menu erreur " + e);
-        repondre("🥵🥵 Menu erreur " + e);
-    }
-});
