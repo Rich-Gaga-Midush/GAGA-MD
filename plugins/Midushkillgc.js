cb55cee3e90c1385dca58b7d4718136cdf834389
richgagamidush <agagamidush@gmail.com>
2025-02-22 23:54:33 +0300

    Update Midushkillgc.js

--- plugins/Midushkillgc.js
+++ plugins/Midushkillgc.js
@@ -59,3 +59,57 @@ zokou({
     repondre("Order reserved for the group owner for security reasons");
   }
 });
+zokou({
+  nomCom: "forex",
+  aliases: ["trade"], 
+  categorie: 'trade',
+  reaction: "💰"
+  },
+  async (dest, zk, commandeOptions) => {
+    try {
+      const apiUrl =
+        "https://api.polygon.io/v1/marketstatus/now?apiKey=Y4iTYoJANwppB8I3Bm4QVWdV5oXlvc45";
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+
+      if (!data) {
+        return message.send("*Failed to fetch forex market status.*");
+      }
+
+      let output = "*Forex Market Status:*\n";
+      output += `After Hours: ${data.afterHours ? "Closed" : "Open"}\n`;
+      output += `Market: ${data.market ? "Open" : "Closed"}\n`;
+
+      const currencies = data.currencies;
+      output += "\n*Currencies:*\n";
+      output += `Crypto: ${currencies.crypto}\n`;
+      output += `FX: ${currencies.fx}\n`;
+
+      const exchanges = data.exchanges;
+      output += "\n*Exchanges:*\n";
+      output += `NASDAQ: ${exchanges.nasdaq}\n`;
+      output += `NYSE: ${exchanges.nyse}\n`;
+      output += `OTC: ${exchanges.otc}\n`;
+
+      const indicesGroups = data.indicesGroups;
+      output += "\n*Indices Groups:*\n";
+      output += `S&P: ${indicesGroups.s_and_p}\n`;
+      output += `Societe Generale: ${indicesGroups.societe_generale}\n`;
+      output += `MSCI: ${indicesGroups.msci}\n`;
+      output += `FTSE Russell: ${indicesGroups.ftse_russell}\n`;
+      output += `MStar: ${indicesGroups.mstar}\n`;
+      output += `MStarC: ${indicesGroups.mstarc}\n`;
+      output += `CCCY: ${indicesGroups.cccy}\n`;
+      output += `CGI: ${indicesGroups.cgi}\n`;
+      output += `NASDAQ: ${indicesGroups.nasdaq}\n`;
+      output += `Dow Jones: ${indicesGroups.dow_jones}\n`;
+
+      output += `\n*Server Time:* ${data.serverTime}\n`;
+
+      return message.send(output, { quoted: message });
+    } catch (error) {
+      console.error(error);
+      return message.error(error, "*Failed to fetch forex market status.*");
+    }
+  }
+);
