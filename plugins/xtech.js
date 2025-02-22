b0f7193706e5f9e94346d17a83b74667be69c3d4
richgagamidush <agagamidush@gmail.com>
2025-02-22 13:51:14 +0300

    Update xtech.js

--- plugins/xtech.js
+++ plugins/xtech.js
@@ -95,7 +95,7 @@ zokou({ nomCom: "menu", categorie: "General" }, async (dest, zk, commandeOptions
  ࿇ *User:*  ${nomAuteurMessage}  
  ࿇ *Time :*  ${temps}
  ࿇ *Date :* ${date} 
- ࿇ *Prefix :* ${s.prefixe}
+ ࿇ *Prefix :* ${settings.PREFIXE} 
  ࿇ *Uptime :*  ${runtime(process.uptime())}  
  ࿇ *plugins :* ${cm.length}
 └───────────────────
@@ -202,7 +202,9 @@ zokou({ nomCom: "list", categorie: "General" }, async (dest, zk, commandeOptions
  ࿇ *User:*  ${nomAuteurMessage}  
  ࿇ *Time :*  ${temps}
  ࿇ *Date :* ${date} 
+ ࿇ *Prefix :* ${settings.PREFIXE} 
  ࿇ *Uptime :*  ${runtime(process.uptime())}  
+ ࿇ *plugins :* ${cm.length}
 └───────────────────
  ━〔 𝐊𝐄𝐄𝐏 𝐔𝐒𝐈𝐍𝐆 𝐆𝐀𝐆𝐀 𝐌𝐃 〕━`;
 
@@ -255,5 +257,109 @@ zokou({ nomCom: "list", categorie: "General" }, async (dest, zk, commandeOptions
         repondre("🥵🥵 Menu erreur " + e);
     }
 });
+zokou({ nomCom: "dashboard", categorie: "General" }, async (dest, zk, commandeOptions) => {
+    let { ms, repondre, prefixe, nomAuteurMessage } = commandeOptions;
+    let { cm } = require(__dirname + "/../framework/zokou");
+    var coms = {};
+    var mode = "public";
 
-        
+    if ((s.MODE).toLocaleLowerCase() != "public") {
+        mode = "Private";
+    }
+
+    // Normalize category to uppercase and organize commands by category
+    cm.map(async (com, index) => {
+        const categoryUpper = com.categorie.toUpperCase();
+        if (!coms[categoryUpper])
+            coms[categoryUpper] = [];
+        coms[categoryUpper].push(com.nomCom);
+    });
+
+    // Set the default timezone from the configuration
+    moment.tz.setDefault('Africa/Nairobi');
+
+    // Create a date and time in the configured timezone
+    const temps = moment().format('HH:mm:ss');
+    const date = moment().format('DD/MM/YYYY');
+
+    // Determine the greeting based on the current time
+    const hour = moment().hour();
+    let greeting = "ʟᴇᴛ's ᴋɪᴄᴋsᴛᴀʀᴛ ʏᴏᴜʀ ᴅᴀʏ ✨";
+    if (hour >= 0 && hour <= 11) {
+        greeting = "ɢᴏᴏᴅ ᴍᴏʀɴɪɴɢ 🌄, ʟᴇᴛ's ᴋɪᴄᴋsᴛᴀʀᴛ ʏᴏᴜʀ ᴅᴀʏ ✨";
+    } else if (hour >= 12 && hour <= 16) {
+        greeting = "ɢᴏᴏᴅ ᴀғᴛᴇʀɴᴏᴏɴ 🌅, ʜᴏᴡ ɪs ʏᴏᴜʀ ᴅᴀʏ ɢᴏɪɴɢ 🎍";
+    } else if (hour >= 16 && hour <= 21) {
+        greeting = "ɢᴏᴏᴅ ᴇᴠᴇɴɪɴɢ 🌃, ɪ ʜᴏᴘᴇ ʏᴏᴜʀ ᴅᴀʏ ᴡᴀs ɢᴏᴏᴅ 🦋 ";
+    } else if (hour >= 21 && hour <= 23) {
+        greeting = "ɢᴏᴏᴅ ɴɪɢʜᴛ🌘, sᴡᴇᴇᴛ ᴅʀᴇᴀᴍs 💫";
+    }
+
+    // Fetch GitHub stats
+    const { totalUsers } = await fetchGitHubStats();
+    const formattedTotalUsers = totalUsers.toLocaleString();
+
+    let infoMsg = `
+> *${greeting}*
+╭━━━〔 𝐗𝐆𝐀𝐆𝐀 𝐁𝐎𝐓𝐒 〕━━━
+┌════════════════⊷
+╭─────────────────
+ ࿇ *Bot name :* XGAGA BOTS
+ ࿇ *User:*  ${nomAuteurMessage}  
+ ࿇ *Time :*  ${temps}
+ ࿇ *Date :* ${date} 
+ ࿇ *Prefix :* ${settings.PREFIXE} 
+ ࿇ *Uptime :*  ${runtime(process.uptime())}  
+ ࿇ *plugins :* ${cm.length}
+└───────────────────
+ ━〔 𝐊𝐄𝐄𝐏 𝐔𝐒𝐈𝐍𝐆 𝐆𝐀𝐆𝐀 𝐌𝐃 〕━`;
+
+    let menuMsg =`
+> © regards\n${readmore} `;
+
+    // Sort categories alphabetically and generate menu
+    const sortedCategories = Object.keys(coms).sort();
+    let commandNumber = 1; 
+
+    for (const cat of sortedCategories) {
+        menuMsg += `
+╭━━━〔  ${cat.toUpperCase()} 〕━━━
+`;
+
+        // Sort commands alphabetically within the category
+        const sortedCommands = coms[cat].sort();
+
+        for (const cmd of sortedCommands) {
+            menuMsg += ` 
+🚇 ${cmd}`;
+        }
+        menuMsg += `
+└───────────────────
+╰═════════════════⊷\n`;
+    }
+
+    menuMsg += `
+> © 𝐊𝐄𝐄𝐏 𝐔𝐒𝐈𝐍𝐆 𝐆𝐀𝐆𝐀 𝐌𝐃`;
+
+    
+    try {
+        await zk.sendMessage(dest, { 
+            text: infoMsg + menuMsg,
+            contextInfo: {
+                mentionedJid: [nomAuteurMessage],
+               showAdAttribution: true,
+                externalAdReply: {
+                    title: "𝐊𝐄𝐄𝐏 𝐔𝐒𝐈𝐍𝐆 𝐆𝐀𝐆𝐀 𝐌𝐃",
+                    body: "𝚁𝙴𝙶𝙰𝚁𝙳𝚂 𝚃𝙾 𝚁𝙸𝙲𝙷-𝙶𝙰𝙶𝙰-𝙼𝙸𝙳𝚄𝚂𝙷",
+                    thumbnailUrl: "https://files.catbox.moe/oznlsw.jpg",
+                    sourceUrl: 'https://whatsapp.com/channel/0029VasnifMFi8xW4Mqysn2F',
+                    mediaType: 1,
+                    renderLargerThumbnail: true
+                }
+            }
+        });
+    } catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+});
